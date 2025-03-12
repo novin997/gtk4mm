6db99a42cfec162e400d832bd44fd8f8cee37d72
novin <cocolupiayi@gmail.com>
2025-03-12 13:31:34 +0800

    feat:test nested grid

--- main.cpp
+++ main.cpp
@@ -11,33 +11,59 @@ public:
 
   Gtk::Grid grid;
 
+  Gtk::Grid boardGrid;
+
   Gtk::Picture picture;
 
+  Gtk::Image image;
+
+  Gtk::Entry entry;
+
   void on_button_clicked();
+
+  void on_text_change();
 };
 
 MyWindow::MyWindow():
   button("Click on me"),
   button_1("Button_1"),
   button_2("Button_2"),
-  picture("./images/steins_gate.jpg")
+  picture("./images/steins_gate.jpg"),
+  image("./images/steins_gate.jpg")
 {
   grid.set_margin(12);
   set_child(grid);
+  grid.set_column_homogeneous(true);
   
-  grid.attach(button,0,0);
+  grid.attach(button,0,0,1);
 
-  grid.attach(button_1,1,0);
+  grid.attach(button_1,1,0,1);
 
   grid.attach(button_2,0,1,2);
 
-  grid.attach(picture,0,2);
+  boardGrid.attach(picture,0,0);
+  boardGrid.attach(image,1,0);
+
+  boardGrid.set_column_homogeneous(true);
+
+  grid.attach(boardGrid,0,2,2);
+
+  grid.attach(entry,0,3);
+
+  entry.set_text("Text");
+  // entry.signal_insert_text().connect(sigc::mem_fun(*this,&MyWindow::on_text_change));
+  entry.signal_changed().connect(sigc::mem_fun(*this,&MyWindow::on_text_change));
 
   button.set_margin(10);
   button.signal_clicked().connect(sigc::mem_fun(*this,&MyWindow::on_button_clicked));
+
   set_title("Anime3x3");
   set_default_size(640, 480);
-}  
+}
+
+void MyWindow::on_text_change() {
+  std::cout << "Text change" << std::endl;
+} 
 
 void MyWindow::on_button_clicked() {
   std::cout << "Button Clicked" << std::endl;
